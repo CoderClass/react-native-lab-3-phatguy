0b34910b0714e49e9217375896bf64a961303e51
Guy Morita <guymorita@gmail.com>
2017-03-30 19:10:15 +0700

    add navigator

--- phatguy/app/app.js
+++ phatguy/app/app.js
@@ -8,6 +8,24 @@ import {
 
 export default class App extends Component {
   render() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('position', position)
+      /*{
+            coords:
+            {
+                speed: -1,
+                longitude: -122.406417,
+                latitude: 37.785834,
+                accuracy: 5,
+                heading: -1,
+                altitude: 0,
+                altitudeAccuracy: -1
+            },
+            timestamp: 1490794016532.687
+      }*/
+    }, (error) => {
+      console.log('error', error);
+    });
     return (
       <View>
         <Text>
